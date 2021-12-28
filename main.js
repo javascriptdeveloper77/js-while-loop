a39e71c2180d493741de04459dc5087bab14ac0f
javascriptdeveloper77 <mnh.webnurhuda@gmail.com>
2021-12-28 21:11:56 +0600

    some modify

--- main.js
+++ main.js
@@ -10,6 +10,25 @@ console.log("Hello");
   }
 }
 
+{
+  let counter = 1;
+
+  while (counter < 5) {
+    console.log("Hello, I am Nurhuda <br>" + " " +  counter);
+    counter++;
+  }
+}
+
+
+{
+  let counter = 5;
+
+  while (counter) {
+    console.log("Hello, I am Nurhuda <br>" + " " + counter);
+    counter--;
+  }
+}
+
 // jokhon same kaj bar bar korte hobe, then use loop
 // loop run korte hole all time true value add korte hbe, false value run hobe
 // condition jotokhon true hobe , totokhon loop cholte thakbe, so condition ke false korte hbe, sei vabe condtion add korte hbe ; 
