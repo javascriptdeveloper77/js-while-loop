04acd4518d4f8d3f17d203ee952f1379cae35df0
nurhuda ashiq <mnh.developernur@gmail.com>
2021-12-28 19:44:25 +0600

    js while loop added

---
+++ main.js
@@ -0,0 +1,35 @@
+"use strict";
+console.log("Hello");
+
+{
+  let counter = 1;
+
+  while (counter < 5) {
+    console.log("Hello, I am Nurhuda <br>");
+    counter++;
+  }
+}
+
+// jokhon same kaj bar bar korte hobe, then use loop
+// loop run korte hole all time true value add korte hbe, false value run hobe
+/* while ( condition) {
+    loop body;
+    varialbe++;
+}
+
+
+// counter < 5;
+// 0 < 5 = true; loop run 
+// 1 < 5 = true ; loop run
+// 2 < 5 = true ; loop run
+// 3 < 5 = true ; loop run
+// 4 < 5 = true ; loop run
+// 5 < 5 = true ; loop run and loop stop 
+// counter++; == per loop 1 kore add hobe 
+*/
+
+// Example
+{
+    let price = 100
+  
+}
