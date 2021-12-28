64036b6fbd16a0e384a1ed9578231cb26c158096
javascriptdeveloper77 <mnh.webnurhuda@gmail.com>
2021-12-28 20:21:52 +0600

    some change in while looop

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ console.log("Hello");
 
 // jokhon same kaj bar bar korte hobe, then use loop
 // loop run korte hole all time true value add korte hbe, false value run hobe
+// condition jotokhon true hobe , totokhon loop cholte thakbe, so condition ke false korte hbe, sei vabe condtion add korte hbe ; 
 /* while ( condition) {
     loop body;
     varialbe++;
@@ -24,12 +25,8 @@ console.log("Hello");
 // 2 < 5 = true ; loop run
 // 3 < 5 = true ; loop run
 // 4 < 5 = true ; loop run
-// 5 < 5 = true ; loop run and loop stop 
+// 5 < 5 = false ;loop stop [ 5 choto noy 5 theke so stop loop]
 // counter++; == per loop 1 kore add hobe 
 */
 
-// Example
-{
-    let price = 100
-  
-}
+
