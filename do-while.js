008dd9f8bd2627ce50279b93d94b8f93fed9bf2b
javascriptdeveloper77 <mnh.webnurhuda@gmail.com>
2021-12-29 19:41:31 +0600

    diff all loop js

---
+++ do-while.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// first do run hobe; then run hote hole while er sorto puron korte hobe
+
+{
+  let doLoop = 1;
+
+  do {
+    console.log("Loop run already" + " " + doLoop);
+    doLoop++;
+  } while (doLoop <= 5);
+}
+
+
+// 
+{
+  let doLoop = 1;
+
+  while (doLoop <= 5) {
+    console.log("Loop while already run" + " " + doLoop);
+    doLoop++;
+  }
+}
